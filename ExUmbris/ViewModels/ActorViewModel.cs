4ed371d064eae8df632f07362d566337ec04e6e3
SaberSnail <16519171+SaberSnail@users.noreply.github.com>
2025-11-01 13:36:04 -0700

    Add basic actor

---
+++ ExUmbris/ViewModels/ActorViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ExUmbris.ViewModels;
+
+public sealed class ActorViewModel : ViewModelBase
+{
+	public ActorViewModel()
+	{
+		Name = "Unknown Actor";
+	}
+
+	public int Id { get; init; }
+	public string Name { get; set; }
+	public MapNodeViewModel? CurrentNode => VerifyAccess(m_currentNode);
+
+	public void MoveToNode(MapNodeViewModel node)
+	{
+		if (CurrentNode == node)
+			return;
+
+		using var _ = ScopedPropertyChange(nameof(CurrentNode));
+
+		if (m_currentNode is not null)
+			m_currentNode.RemoveActor(this);
+		m_currentNode = node;
+		if (m_currentNode is not null)
+			m_currentNode.AddActor(this);
+	}
+
+	private MapNodeViewModel? m_currentNode;
+}
--- ExUmbris/ViewModels/MapNodeViewModel.cs
+++ ExUmbris/ViewModels/MapNodeViewModel.cs
@@ -6,13 +6,37 @@ public sealed class MapNodeViewModel : ViewModelBase
 {
 	public MapNodeViewModel()
 	{
-		Name = "Unknown";
+		Name = "Unknown Map Node";
 		Coordinates = new(0.0, 0.0);
 		ConnectedNodes = [];
+		m_actors = [];
 	}
 
 	public int Id { get; init; }
 	public string Name { get; set; }
 	public MapCoordinates Coordinates { get; set; }
 	public IReadOnlyList<MapNodeViewModel> ConnectedNodes { get; set; }
+	public IReadOnlyList<ActorViewModel> Actors => VerifyAccess(m_actors);
+
+	public void RemoveActor(ActorViewModel actor)
+	{
+		VerifyAccess();
+		if (m_actors.Contains(actor))
+		{
+			using var _ = ScopedPropertyChange(nameof(Actors));
+			m_actors.Remove(actor);
+		}
+	}
+
+	public void AddActor(ActorViewModel actor)
+	{
+		VerifyAccess();
+		if (!m_actors.Contains(actor))
+		{
+			using var _ = ScopedPropertyChange(nameof(Actors));
+			m_actors.Add(actor);
+		}
+	}
+
+	private readonly List<ActorViewModel> m_actors;
 }
